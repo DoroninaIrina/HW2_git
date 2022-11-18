faa479a085c3b6bb59bf6727c699c0fab0f3600e
Irina Doronina <doroninairinaa@rambler.ru>
2022-11-18 18:16:46 +0300

    Just to create a conflict

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello qa.guru!");
-        System.out.println("The second home work");
+        System.out.println("The second home work with a conflict");
     }
 }
